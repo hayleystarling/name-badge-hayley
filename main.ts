65ad01bd8b7638a56ed6c9e48f10359d767d2c1d
hayleystarling <68075946+hayleystarling@users.noreply.github.com>
2020-07-09 14:25:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("I am Hayley")
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showString("Hey!")
+basic.showIcon(IconNames.Square)
+basic.showIcon(IconNames.SmallSquare)
+basic.showIcon(IconNames.SmallDiamond)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+basic.showString("What’s up?")
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+	
+})
